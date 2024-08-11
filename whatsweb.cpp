73c37362b938511c06ce3ec0b1a5be8a3c1ede05
Tentomushi <102291552+thetentomushi@users.noreply.github.com>
2024-08-11 12:34:02 +0530

    Update whatsweb.cpp

--- whatsweb.cpp
+++ whatsweb.cpp
@@ -29,7 +29,7 @@ private:
 };
 
 int main(int argc, char* argv[]) {
-    auto app = Gtk::Application::create(argc, argv, "org.gtkmm.example");
+    auto app = Gtk::Application::create(argc, argv, "org.whatsapp.web");
 
     WhatsAppWindow window;
 
